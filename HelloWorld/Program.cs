b6150bf7acae7caa73b483bfe877189735ebbbb2
CLux <cplux7@gmail.com>
2019-05-13 15:36:20 -0400

    first Commit

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HelloWorld!");
+            Console.WriteLine("I can't let you do that Dave...");
+            Console.WriteLine("What do you mean HAL?");
+        }
+    }
+}
